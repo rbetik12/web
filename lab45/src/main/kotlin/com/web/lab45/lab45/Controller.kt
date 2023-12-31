a16f883eb64a572ccc624b0dd03dbc0f221828e6
Vitaliy Prikota <mr.prikota@gmail.com>
2023-12-31 16:05:39 +0300

    Added authentication to rest methods and renamed endpoints

--- lab45/src/main/kotlin/com/web/lab45/lab45/Controller.kt
+++ lab45/src/main/kotlin/com/web/lab45/lab45/Controller.kt
@@ -1,5 +1,7 @@
 package com.web.lab45.lab45
 
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,16 +9,35 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api/rest")
+@RequestMapping("api/product")
 class Controller(private val repo: ProductRepo) {
-    @PostMapping("new")
-    fun create(@RequestBody product: ProductDTO): ResponseEntity<ProductDTO> {
+    @PostMapping("auth")
+    fun auth(@RequestBody user: User, request: HttpServletRequest): ResponseEntity<Unit> {
+        if (user.username == "user" && user.password == "password") {
+            request.session.invalidate()
+            val session = request.getSession(true)
+            return ResponseEntity.ok().build()
+        }
+
+        return ResponseEntity.badRequest().build()
+    }
+
+    @PostMapping
+    fun create(@RequestBody product: ProductDTO, request: HttpServletRequest): ResponseEntity<ProductDTO> {
+        if (request.getSession(false) == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
         val newEntity = repo.save(product)
         return ResponseEntity.ok().body(newEntity)
     }
 
-    @PutMapping("update")
-    fun update(@RequestBody product: ProductDTO): ResponseEntity<ProductDTO> {
+    @PutMapping
+    fun update(@RequestBody product: ProductDTO, request: HttpServletRequest): ResponseEntity<ProductDTO> {
+        if (request.getSession(false) == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
         val productOpt = repo.findById(product.id)
 
         if (productOpt.isEmpty)
@@ -28,8 +49,12 @@ class Controller(private val repo: ProductRepo) {
         return ResponseEntity.ok().body(updatedProduct)
     }
 
-    @GetMapping("/{id}")
-    fun read(@PathVariable id: Long): ResponseEntity<ProductDTO> {
+    @GetMapping("{id}")
+    fun read(@PathVariable id: Long, request: HttpServletRequest): ResponseEntity<ProductDTO> {
+        if (request.getSession(false) == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
         val productOpt = repo.findById(id)
 
         if (productOpt.isEmpty)
@@ -40,8 +65,12 @@ class Controller(private val repo: ProductRepo) {
         return ResponseEntity.ok().body(productOpt.get())
     }
 
-    @DeleteMapping("/delete/{id}")
-    fun delete(@PathVariable id: Long): ResponseEntity<Unit> {
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: Long, request: HttpServletRequest): ResponseEntity<Unit> {
+        if (request.getSession(false) == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
         val productOpt = repo.findById(id)
 
         if (productOpt.isEmpty)
---
+++ lab45/src/main/kotlin/com/web/lab45/lab45/User.kt
@@ -0,0 +1,3 @@
+package com.web.lab45.lab45
+
+data class User(val username: String, val password: String)
